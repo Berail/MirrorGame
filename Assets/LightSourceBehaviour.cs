d06fcff233c3f088752893035a3a2f2d5fa70803
Berail <berail@windowslive.com>
2016-03-23 19:04:34 +0100

    Modyfied code for LightSource to instantiate proper LightRayGO
    Need to change it to instantiate ray from class maybe ?

--- Assets/LightSourceBehaviour.cs
+++ Assets/LightSourceBehaviour.cs
@@ -1,29 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LightSourceBehaviour : MonoBehaviour {
+public class LightSourceBehaviour : MonoBehaviour
+{
 
+    public GameObject lightRayPrefab;
+
+    private GameObject lightRayInstantiateRay;
     private Vector3 LightDirectionVector;
     private Ray ray;
     private RaycastHit hit;
     private LightRay lightRay;
-    private float raydistance =100;
-    
+    private float raydistance = 100;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         LightDirectionVector = this.transform.TransformDirection(transform.forward);
         lightRay = new LightRay();
-	}
-	
+        lightRayInstantiateRay = (GameObject) Instantiate(lightRayPrefab, this.transform.position, lightRayPrefab.transform.rotation);
+    }
+
     //TODO: Delegate creating new ray to mirror object.
 
-	// Update is called once per frame
-	void Update () {
-        Debug.DrawRay(transform.position, LightDirectionVector* raydistance, Color.red);
-	    if(Physics.Raycast(transform.position, LightDirectionVector, out hit, raydistance))
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, LightDirectionVector * raydistance, Color.red);
+
+        if (Physics.Raycast(transform.position, LightDirectionVector, out hit, raydistance))
         {
-            raydistance = Vector3.Distance(hit.point, this.transform.position);
+
+            raydistance = Vector3.Distance(hit.point, this.transform.position) + 0.1f;
+
+
+
             Debug.Log("HIT!" + "\n" + "Normal Vector" + hit.normal);
             Debug.DrawRay(hit.point, hit.normal, Color.green);
             //Vector odbity
@@ -34,5 +45,8 @@ public class LightSourceBehaviour : MonoBehaviour {
         {
             raydistance = 100;
         }
-	}
+
+        lightRayInstantiateRay.transform.localScale = new Vector3(lightRayInstantiateRay.transform.localScale.x, raydistance, lightRayInstantiateRay.transform.localScale.z);
+
+    }
 }
